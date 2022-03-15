b71a890990418e5602c2a5df29defc921eec795c
Phil <happy.melodist@gmail.com>
2022-03-14 21:33:23 -0500

    write the skeletons for the Game and Enemy classes, the animate function, and place all code inside of a load event listener

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 // Aim for excellence, not perfection
+document.addEventListener('load', function(){
+  const canvas = document.querySelector("#icythein");
+const ctx = canvas.getContext("2d");
+canvas.width = 500;
+canvas.height = 800;
+
+class Game {
+  constructor(){
+    this.enemies = [];
+
+  }
+  update(){
+
+  }
+  draw(){
+    #addNewEnemy(){
+
+    }
+  }
+}
+
+class Enemy {
+  constructor() {
+
+  }
+  update() {
+
+  }
+  draw() {
+
+  }
+}
+
+function animate () {
+  ctx.clearRect(0,0,canvas.width,canvas.height)
+  // our delicious code
+  requestAnimationFrame(animate)
+
+
+}
+})
