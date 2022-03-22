20f76350603ad3fca5b94d9d80cd99e4b2ecb680
Phil <happy.melodist@gmail.com>
2022-03-22 08:21:07 -0500

    check for enemies outside of the canvas and filter them from the enemies array

--- script.js
+++ script.js
@@ -3,47 +3,71 @@
 
 // });
 
+// document.addEventListener("DOMContentLoaded", function () {
+
+// });
+
 const canvas = document.querySelector("#icythein");
 const ctx = canvas.getContext("2d");
 canvas.width = 500;
 canvas.height = 800;
 
 class Game {
-  constructor() {
+  constructor(ctx, width, height) {
+    this.ctx = ctx;
+    this.width = width;
+    this.height = height;
     this.enemies = [];
-    this.#addNewEnemy();
-    console.log(this.enemies);
+    this.enemyInterval = 1000;
+    this.enemyTimer = 0;
   }
-  update() {}
-  draw() {}
+  update(deltaTime) {
+    this.enemies = this.enemies.filter((boobie) => !boobie.markedForDeath);
+    if (this.enemyTimer > this.enemyInterval) {
+      this.#addNewEnemy();
+      this.enemyTimer = 0;
+      console.log(this.enemies);
+    } else {
+      this.enemyTimer += deltaTime;
+    }
+    this.enemies.forEach((boobie) => boobie.update());
+  }
+  draw() {
+    this.enemies.forEach((boobie) => boobie.draw(this.ctx));
+  }
   #addNewEnemy() {
-    this.enemies.push(new Enemy());
+    this.enemies.push(new Enemy(this));
   }
 }
 
 class Enemy {
-  constructor() {
-    this.x = 100;
-    this.y = 100;
+  constructor(game) {
+    this.game = game;
+    console.log(this.game);
+    this.x = this.game.width;
+    this.y = Math.random() * this.game.height;
     this.width = 100;
     this.height = 100;
+    this.markedForDeath = false;
   }
   update() {
     --this.x;
+    // this is where we remove the enemies
+    if (this.x < 0 - this.width) this.markedForDeath = true;
   }
-  draw() {
+  draw(ctx) {
     ctx.fillRect(this.x, this.y, this.width, this.height);
   }
 }
 
-const game = new Game();
+const game = new Game(ctx, canvas.width, canvas.height);
 let lastTime = 1;
 function animate(timeStamp) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   const deltaTime = timeStamp - lastTime;
   lastTime = timeStamp;
-
-  // our delicious code
+  game.update(deltaTime);
+  game.draw();
   requestAnimationFrame(animate);
 }
 
