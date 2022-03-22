6aa6b944c40e47fce5c570968092f458d210cda1
Phil <happy.melodist@gmail.com>
2022-03-22 06:53:45 -0500

    complete project through the logging of the push of the first enemy into the Game class enemies array

--- script.js
+++ script.js
@@ -1,44 +1,52 @@
 // Aim for excellence, not perfection
-document.addEventListener('load', function(){
-  const canvas = document.querySelector("#icythein");
+// document.addEventListener("load", function () {
+
+// });
+
+const canvas = document.querySelector("#icythein");
 const ctx = canvas.getContext("2d");
 canvas.width = 500;
 canvas.height = 800;
 
 class Game {
-  constructor(){
+  constructor() {
     this.enemies = [];
-
+    this.#addNewEnemy();
+    console.log(this.enemies);
   }
-  update(){
-
-  }
-  draw(){
-    #addNewEnemy(){
-
-    }
+  update() {}
+  draw() {}
+  #addNewEnemy() {
+    this.enemies.push(new Enemy());
   }
 }
 
 class Enemy {
   constructor() {
-
+    this.x = 100;
+    this.y = 100;
+    this.width = 100;
+    this.height = 100;
   }
   update() {
-
+    --this.x;
   }
   draw() {
-
+    ctx.fillRect(this.x, this.y, this.width, this.height);
   }
 }
 
-function animate () {
-  ctx.clearRect(0,0,canvas.width,canvas.height)
+const game = new Game();
+let lastTime = 1;
+function animate(timeStamp) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  const deltaTime = timeStamp - lastTime;
+  lastTime = timeStamp;
+
   // our delicious code
-  requestAnimationFrame(animate)
-
-
+  requestAnimationFrame(animate);
 }
-})
+
+animate(0);
 
 // skeleton completed March 15, 2022
