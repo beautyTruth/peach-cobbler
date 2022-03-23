7323fe641904f914f47744a596b27a99ae2ac6e4
Phil <happy.melodist@gmail.com>
2022-03-23 00:01:43 -0500

    complete project by marking for deletion the spider enemy

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ class Game {
     this.enemies = [];
     this.enemyInterval = 500;
     this.enemyTimer = 0;
-    this.enemyTipi = ["worm", "ghost"];
+    this.enemyTipi = ["worm", "ghost", "spider"];
   }
   update(deltaTime) {
     this.enemies = this.enemies.filter((boobie) => !boobie.markedForDeath);
@@ -42,6 +42,7 @@ class Game {
     // console.log(randomEnemy);
     if (randomEnemy == "worm") this.enemies.push(new Worm(this));
     else if (randomEnemy == "ghost") this.enemies.push(new Ghost(this));
+    else if (randomEnemy == "spider") this.enemies.push(new Spider(this));
     // this.enemies.sort((a, b) => a.y - b.y);
   }
 }
@@ -49,21 +50,28 @@ class Game {
 class Enemy {
   constructor(game) {
     this.game = game;
-    this.x = this.game.width;
-    this.y = Math.random() * this.game.height;
-    this.width = 100;
-    this.height = 100;
     this.markedForDeath = false;
+    this.frameX = 0;
+    this.maxFrame = 5;
+    this.frameInterval = 100;
+    this.frameTimer = 0;
   }
   update(deltaTime) {
     this.x -= this.vx * deltaTime;
     // this is where we remove the enemies
     if (this.x < 0 - this.width) this.markedForDeath = true;
+    if (this.frameTimer > this.frameInterval) {
+      if (this.frameX < this.maxFrame) this.frameX++;
+      else this.frameX = 0;
+      this.frameTimer = 0;
+    } else {
+      this.frameTimer += deltaTime;
+    }
   }
   draw(ctx) {
     ctx.drawImage(
       this.image,
-      0,
+      this.frameX * this.spriteWidth,
       0,
       this.spriteWidth,
       this.spriteHeight,
@@ -88,17 +96,32 @@ class Worm extends Enemy {
     this.vx = Math.random() * 0.1 + 0.1;
   }
 }
-class Worm extends Enemy {
+class Spider extends Enemy {
   constructor(game) {
     super(game);
-    this.spriteWidth = 229;
-    this.spriteHeight = 171;
+    this.spriteWidth = 310;
+    this.spriteHeight = 175;
     this.width = this.spriteWidth / 2;
     this.height = this.spriteHeight / 2;
-    this.x = this.game.width;
-    this.y = this.game.height - this.height;
-    this.image = worm;
-    this.vx = Math.random() * 0.1 + 0.1;
+    this.x = Math.random() * this.game.width;
+    this.y = 0 - this.height;
+    this.image = spider;
+    this.vx = 0;
+    this.vy = Math.random() * 0.1 + 0.1;
+    this.maxLength = Math.random() * this.game.height;
+  }
+  update(deltaTime) {
+    super.update(deltaTime);
+    if (this.y < 0 - this.height * 2) this.markedForDeath = true;
+    this.y += this.vy * deltaTime;
+    if (this.y > this.maxLength) this.vy *= -1;
+  }
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.moveTo(this.x + this.width / 2, 0);
+    ctx.lineTo(this.x + this.width / 2, this.y + 10);
+    ctx.stroke();
+    super.draw(ctx);
   }
 }
 class Ghost extends Enemy {
@@ -120,7 +143,7 @@ class Ghost extends Enemy {
     this.y += Math.sin(this.angle) * this.curve;
     this.angle += 0.04;
   }
-  draw() {
+  draw(ctx) {
     // ctx.save();
     ctx.globalAlpha = 0.5;
     super.draw(ctx);
