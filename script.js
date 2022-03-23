ebbf483cd653fedb1e9dadf9f0c70e825c7a1fa5
Phil <happy.melodist@gmail.com>
2022-03-22 23:03:05 -0500

    complete project through the customization of movement of the ghost child class

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ class Game {
     this.width = width;
     this.height = height;
     this.enemies = [];
-    this.enemyInterval = 100;
+    this.enemyInterval = 500;
     this.enemyTimer = 0;
     this.enemyTipi = ["worm", "ghost"];
   }
@@ -88,6 +88,19 @@ class Worm extends Enemy {
     this.vx = Math.random() * 0.1 + 0.1;
   }
 }
+class Worm extends Enemy {
+  constructor(game) {
+    super(game);
+    this.spriteWidth = 229;
+    this.spriteHeight = 171;
+    this.width = this.spriteWidth / 2;
+    this.height = this.spriteHeight / 2;
+    this.x = this.game.width;
+    this.y = this.game.height - this.height;
+    this.image = worm;
+    this.vx = Math.random() * 0.1 + 0.1;
+  }
+}
 class Ghost extends Enemy {
   constructor(game) {
     super(game);
@@ -99,6 +112,20 @@ class Ghost extends Enemy {
     this.y = Math.random() * this.game.height * 0.6;
     this.image = ghost;
     this.vx = Math.random() * 0.2 + 0.1;
+    this.angle = 0;
+    this.curve = Math.random() * 3;
+  }
+  update(deltaTime) {
+    super.update(deltaTime);
+    this.y += Math.sin(this.angle) * this.curve;
+    this.angle += 0.04;
+  }
+  draw() {
+    // ctx.save();
+    ctx.globalAlpha = 0.5;
+    super.draw(ctx);
+    // ctx.restore();
+    ctx.globalAlpha = 1;
   }
 }
 
