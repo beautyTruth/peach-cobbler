30bc722e7d31cb8758abd6c89d2e131795a52cb1
Phil <happy.melodist@gmail.com>
2022-03-22 21:47:29 -0500

    progress through the application of delta time to the randomly released enemies which are at this point still just the first frame of the worm

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ class Game {
     this.width = width;
     this.height = height;
     this.enemies = [];
-    this.enemyInterval = 1000;
+    this.enemyInterval = 100;
     this.enemyTimer = 0;
   }
   update(deltaTime) {
@@ -30,7 +30,7 @@ class Game {
     } else {
       this.enemyTimer += deltaTime;
     }
-    this.enemies.forEach((boobie) => boobie.update());
+    this.enemies.forEach((boobie) => boobie.update(deltaTime));
   }
   draw() {
     this.enemies.forEach((boobie) => boobie.draw(this.ctx));
@@ -43,33 +43,43 @@ class Game {
 class Enemy {
   constructor(game) {
     this.game = game;
-    // console.log(this.game);
     this.x = this.game.width;
     this.y = Math.random() * this.game.height;
     this.width = 100;
     this.height = 100;
     this.markedForDeath = false;
   }
-  update() {
-    --this.x;
+  update(deltaTime) {
+    this.x -= this.vx * deltaTime;
     // this is where we remove the enemies
     if (this.x < 0 - this.width) this.markedForDeath = true;
   }
   draw(ctx) {
-    // ctx.fillRect(this.x, this.y, this.width, this.height);
-    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    ctx.drawImage(
+      this.image,
+      0,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
 
 class Worm extends Enemy {
   constructor(game) {
     super(game);
+    this.spriteWidth = 229;
+    this.spriteHeight = 171;
+    this.width = this.spriteWidth / 2;
+    this.height = this.spriteHeight / 2;
     this.x = this.game.width;
     this.y = Math.random() * this.game.height;
-    this.width = 200;
-    this.height = 100;
     this.image = worm;
-    console.log(this.image);
+    this.vx = Math.random() * 0.1 + 0.1;
   }
 }
 
